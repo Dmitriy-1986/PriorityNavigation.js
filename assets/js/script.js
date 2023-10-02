d3f3d9cfa2c33ca2d7c7d26513eccad16a85ebf0
Dmitriy-1986 <dovgal.dima.86@gmail.com>
2023-10-02 20:25:27 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var wrapper = document.querySelector(".nav-wrapper");
+var nav = priorityNav.init({
+    mainNavWrapper: ".nav-wrapper",
+    mainNav: ".nav-ul",
+    navDropdownLabel: 'test',
+    navDropdownClassName: "nav__dropdown",
+    navDropdownToggleClassName: "nav__dropdown-toggle",
+});
